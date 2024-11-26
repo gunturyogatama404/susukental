b1de638b68ffc3b5bbc9a46b0e671bde6125a6f9
gunturyogatama404 <99540625+gunturyogatama404@users.noreply.github.com>
2024-11-27 06:17:37 +0700

    Update amay.py

--- utils/amay.py
+++ utils/amay.py
@@ -1,123 +1,162 @@
 import time
 import random
 
-
 class Solver:
-    def __init__(self, playwright, proxy=None, headless=True):
+    def __init__(self, playwright, proxy="722700435849273f5e46:189739668b422895@gw.dataimpulse.com:823", headless=False):
         self.playwright = playwright
         self.proxy = proxy
         self.headless = headless
-        self.current_x = 0
-        self.current_y = 0
-        self.start_browser()
 
-    def start_browser(self):
-        # Proxy setup
-        if self.proxy:
-            try:
-                username, password_and_host = self.proxy.split(":")
-                password, host = password_and_host.split("@")
-                self.browser = self.playwright.chromium.launch(
-                    headless=self.headless,
-                    proxy={"server": f"http://{host}", "username": username, "password": password},
-                )
-            except Exception as e:
-                raise ValueError(f"Invalid proxy format: {self.proxy}. Error: {e}")
-        else:
-            self.browser = self.playwright.chromium.launch(headless=self.headless)
-
+        self.start_browser(self.playwright)
+            
     def terminate(self):
-        if hasattr(self, "browser"):
-            self.browser.close()
+        self.browser.close()
 
     def build_page_data(self):
-        # Build custom page
+        # this builds a custom page with the sitekey so we do not have to load the actual page, taking less bandwidth
         with open("utils/page.html") as f:
             self.page_data = f.read()
-        stub = f'<div class="cf-turnstile" data-sitekey="{self.sitekey}"></div>'
+        stub = f"<div class=\"cf-turnstile\" data-sitekey=\"{self.sitekey}\"></div>"
         self.page_data = self.page_data.replace("<!-- cf turnstile -->", stub)
 
     def get_mouse_path(self, x1, y1, x2, y2):
+        # calculate the path to x2 and y2 from x1 and y1
         path = []
-        x, y = x1, y1
-
+        x = x1
+        y = y1
         while abs(x - x2) > 3 or abs(y - y2) > 3:
             diff = abs(x - x2) + abs(y - y2)
-            speed = random.uniform(0.5, 2) * (diff / 45 if diff > 20 else 1)
+            speed = random.randint(1, 2)
+            if diff < 20:
+                speed = random.randint(1, 3)
+            else:
+                speed *= diff / 45
 
             if abs(x - x2) > 3:
-                x += speed if x < x2 else -speed
+                if x < x2:
+                    x += speed
+                elif x > x2:
+                    x -= speed
             if abs(y - y2) > 3:
-                y += speed if y < y2 else -speed
-
-            path.append((round(x), round(y)))
+                if y < y2:
+                    y += speed
+                elif y > y2:
+                    y -= speed
+            path.append((x, y))
 
         return path
 
     def move_to(self, x, y):
-        for px, py in self.get_mouse_path(self.current_x, self.current_y, x, y):
-            self.page.mouse.move(px, py)
-            time.sleep(random.uniform(0.002, 0.01))
-        self.current_x, self.current_y = x, y
-
-    def find_element_value(self, selector):
-        elem = self.page.query_selector(selector)
-        return elem.get_attribute("value") if elem else None
+        for path in self.get_mouse_path(self.current_x, self.current_y, x, y):
+            self.page.mouse.move(path[0], path[1])
+            if random.randint(0, 100) > 15:
+                time.sleep(random.randint(1, 5) / random.randint(400, 600))
 
     def solve_invisible(self):
-        for _ in range(10):
-            self.move_to(random.randint(0, self.window_width), random.randint(0, self.window_height))
-            value = self.find_element_value("[name=cf-turnstile-response]")
-            if value:
-                return value
-            time.sleep(random.uniform(0.005, 0.015))
+        iterations = 0
+
+        while iterations < 10:
+            self.random_x = random.randint(0, self.window_width)
+            self.random_y = random.randint(0, self.window_height)
+            iterations += 1
+
+            self.move_to(self.random_x, self.random_y)
+            self.current_x = self.random_x
+            self.current_y = self.random_y
+            elem = self.page.query_selector("[name=cf-turnstile-response]")
+            if elem:
+                if elem.get_attribute("value"):
+                    return elem.get_attribute("value")
+            time.sleep(random.randint(2, 5) / random.randint(400, 600))
         return "failed"
+            
 
     def solve_visible(self):
+     
         iframe = self.page.query_selector("iframe")
         while not iframe:
-            time.sleep(0.1)
             iframe = self.page.query_selector("iframe")
-
+            time.sleep(0.1)
         while not iframe.bounding_box():
             time.sleep(0.1)
 
-        iframe_box = iframe.bounding_box()
-        self.move_to(
-            iframe_box["x"] + random.randint(5, 12),
-            iframe_box["y"] + random.randint(5, 12),
-        )
+        x = iframe.bounding_box()["x"] + random.randint(5, 12)
+        y = iframe.bounding_box()["y"] + random.randint(5, 12)
+        self.move_to(x, y)
+        self.current_x = x
+        self.current_y = y
         framepage = iframe.content_frame()
         checkbox = framepage.query_selector("input")
+
         while not checkbox:
-            time.sleep(0.1)
             checkbox = framepage.query_selector("input")
+            time.sleep(0.1)
 
-        checkbox_box = checkbox.bounding_box()
-        if checkbox_box:
-            x = checkbox_box["x"] + random.uniform(checkbox_box["width"] * 0.2, checkbox_box["width"] * 0.8)
-            y = checkbox_box["y"] + random.uniform(checkbox_box["height"] * 0.2, checkbox_box["height"] * 0.8)
-            self.move_to(x, y)
-            time.sleep(random.uniform(0.1, 0.3))
-            self.page.mouse.click(x, y)
+        width = checkbox.bounding_box()["width"]
+        height = checkbox.bounding_box()["height"]
 
-        return self.solve_invisible()
+        x = checkbox.bounding_box()["x"] + width / 5 + random.randint(int(width / 5), int(width - width / 5))
+        y = checkbox.bounding_box()["y"] + height / 5 + random.randint(int(height / 5), int(height - height / 5))
 
+        self.move_to(x, y)
+
+        self.current_x = x
+        self.current_y = y
+        
+
+        time.sleep(random.randint(1, 5) / random.randint(400, 600))
+        self.page.mouse.click(x, y)
+
+        iterations = 0
+
+        while iterations < 10:
+            self.random_x = random.randint(0, self.window_width)
+            self.random_y = random.randint(0, self.window_height)
+            iterations += 1
+
+            self.move_to(self.random_x, self.random_y)
+            self.current_x = self.random_x
+            self.current_y = self.random_y
+            elem = self.page.query_selector("[name=cf-turnstile-response]")
+            if elem:
+                if elem.get_attribute("value"):
+                    return elem.get_attribute("value")
+            time.sleep(random.randint(2, 5) / random.randint(400, 600))
+        return "failed"
+
+
+    
     def solve(self, url, sitekey, invisible=False):
-        self.url = url if url.endswith("/") else url + "/"
+        self.url = url + "/" if not url.endswith("/") else url
         self.sitekey = sitekey
         self.invisible = invisible
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
 
         self.build_page_data()
+        
         self.page.route(self.url, lambda route: route.fulfill(body=self.page_data, status=200))
         self.page.goto(self.url)
+        output = "failed"
+        self.current_x = 0
+        self.current_y = 0
 
         self.window_width = self.page.evaluate("window.innerWidth")
         self.window_height = self.page.evaluate("window.innerHeight")
+        if self.invisible:
+            output = self.solve_invisible()
+        else:
+            output = self.solve_visible()
+        
+        self.context.close()
+        return output
+    def start_browser(self, playwright):
 
-        try:
-            return self.solve_invisible() if invisible else self.solve_visible()
-        finally:
-            self.context.close()
+        if self.proxy:
+            self.browser = playwright.chromium.launch(headless=self.headless, proxy={
+                "server": "http://" + self.proxy.split("@")[1],
+                "username": self.proxy.split("@")[0].split(":")[0],
+                "password": self.proxy.split("@")[0].split(":")[1]
+            })
+        else:
+            self.browser = playwright.chromium.launch(headless=self.headless)
